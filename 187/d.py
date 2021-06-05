0772ba9d72d3f3a446029262b8f303e91e9c0fae
TeppeiSudo <sudo20.t.ab@gmail.com>
2021-06-05 14:25:40 +0900

    add 187/d

---
+++ 187/d.py
@@ -0,0 +1,15 @@
+N = int(input())
+A, B, C = [0]*N, [0]*N, [0]*N
+for n in range(N):
+    A[n], B[n] = map(int, input().split())
+    C[n] = 2*A[n] + B[n]
+
+
+C.sort(reverse=True)
+diff = sum(A)
+count = ans = 0
+while count <= diff:
+    count += C[ans]
+    ans += 1
+
+print(ans)
